76a2d7dc9e892add419dc91202f655055511a81f
Latif Şimşek <simseklatif5496@gmail.com>
2025-10-02 12:31:52 +0300

    Add data validation configuration and implement validation pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from text_summarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_summarization.logging import logger
-
+from text_summarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -14,3 +14,13 @@ except Exception as e:
     raise e
 
 
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
