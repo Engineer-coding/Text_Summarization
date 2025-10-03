8b9a2cdb47aa3fa51af54c7f8f8cc00d185c77d4
Latif Şimşek <simseklatif5496@gmail.com>
2025-10-03 14:33:18 +0300

    Add evaluation strategy and logging parameters to ModelTrainerConfig; implement PredictionPipeline class

---
+++ src/text_summarization/pipeline/prediction.py
@@ -0,0 +1,7 @@
+from text_summarization.config.configuration import ConfigurationManager
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluation_config()
